05098bd392ae22fe6c850df8be7dd38dfc970ffc
Kevin McPhillips <github@kevinmcphillips.ca>
2013-09-30 21:52:22 -0500

    add factory and specs

--- Gemfile
+++ Gemfile
@@ -12,4 +12,5 @@ group :development, :test do
   gem 'thin'
 
   gem 'rspec-rails'
+  gem 'factory_girl_rails'
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe Customer do
+  let(:customer){ FactoryGirl.create(:customer) }
+  let(:customer_jones){ FactoryGirl.create(:customer, first_name: nil, last_name: "Jones") }
+
+  describe "#full_name" do
+    it "should join the first and last name" do
+      expect(customer.full_name).to eq("John Smith")
+    end
+
+    it "should only show the names present" do
+      expect(customer_jones.full_name).to eq("Jones")
+    end
+  end
+
+  describe "#anonymous?" do
+    it "should not be anonymous if it has a #full_name" do
+      expect(customer).to_not be_anonymous
+    end
+  end
 
 end
