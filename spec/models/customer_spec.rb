1159e54205ddcb795c414cec3d1cbc65066b8bbe
Kevin McPhillips <github@kevinmcphillips.ca>
2013-10-13 11:47:31 -0500

    Clean up descriptions

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -5,25 +5,23 @@ describe Customer do
   let(:customer_jones){ FactoryGirl.create(:customer, first_name: nil, last_name: "Jones") }
 
   describe "#full_name" do
-    it "should join the first and last name" do
+    it "should join together the first and last names" do
       expect(customer.full_name).to eq("John Smith")
     end
 
-    it "should only show the names present" do
+    it "should only output the names if they are present" do
       expect(customer_jones.full_name).to eq("Jones")
     end
   end
 
   describe "#anonymous?" do
-    it "should not be anonymous if it has a #full_name" do
+    it "should be false if the customer has a #full_name" do
       expect(customer).to_not be_anonymous
     end
 
-    it "should be anonymous if it does not have a #full_name" do
+    it "should be true if the customer does not have a #full_name" do
       customer.should_receive(:full_name).and_return('')
       expect(customer).to be_anonymous
     end
   end
-
-
 end
